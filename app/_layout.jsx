c7f11ff9ee222923f50666811e79303fcbdb8672
gouderhicham <gouderhicham@gmail.com>
2024-08-06 12:36:28 +0100

    navigatiom setup

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,18 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
 const Layout = () => {
   return (
     <View>
       <Stack>
-        <Stack.Screen name='index' /> 
-        <Stack.Screen name='users' /> 
-        <Stack.Screen name='posts' /> 
+        <Stack.Screen name="index" options={{ headerTitle: "index" }} />
+        <Stack.Screen name="users" options={{ headerTitle: "index" }} />
+        <Stack.Screen name="posts" options={{ headerTitle: "index" }} />
       </Stack>
     </View>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
